835c47c7ea2d1ad36c22edf4f4fe978a91a04335
Federico Gianno <giannofederico@gmail.com>
2017-11-13 11:46:05 +0100

    Update happy_wedding.c

--- happy_wedding.c
+++ happy_wedding.c
@@ -1,4 +1,4 @@
-/*  Copyright 2017 Federico Gianno
+/*  Copyright © 2017 Federico Gianno
 
     happy-marriage is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -11,9 +11,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with happy-marriage. If not, see <http://www.gnu.org/licenses/>.
-
-    Developed by Nindo */
+    along with happy-marriage. If not, see <http://www.gnu.org/licenses/>. */
 
 
 #include <stdio.h>
